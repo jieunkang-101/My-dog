12fd52f714f952fb86048b5358da228c65abb47d
Student <example@example.com>
2020-01-14 10:18:37 -0800

    update Java Script

---
+++ js/scripts.js
@@ -0,0 +1,10 @@
+$(document).ready(function() {
+  $("button#green").click(function() {
+    $("body").addClass("green-background");
+  });
+
+  $("button#yellow").click(function() {
+    $("body").removeClass();
+  });
+});
+
